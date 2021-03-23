2e30a0c083fe5d3a790d3eabbc89d66499facd89
miciooo <mitchell2.wilson@live.uwe.ac.uk>
2021-03-23 15:53:26 +0000

    Button Animation
    finally got the button to trigger the animation!!

---
+++ AR Scrapbook App/Assets/virtualbutton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class virtualbutton : MonoBehaviour
+{
+    public GameObject model;
+    VirtualButtonBehaviour[] awa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        awa = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < awa.Length; ++i)
+        {
+            awa[i].RegisterOnButtonPressed(OnButtonPressed);
+            awa[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+        model.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour go
+        )
+    {
+        model.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour go
+        )
+    {
+        model.SetActive(false);
+    }
+
+}
